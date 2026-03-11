dcabc858f1d0a0a67a2f17de27556c95d8262b36
Pavlo Arsiievych <pavlo.arsiievych@onix-systems.com>
2026-03-11 19:54:28 +0200

    feat(nav-bar): Add nav-bar component with styles and integrate into app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,14 @@
 import {Component} from "@angular/core";
 import {Header} from "./header/header";
+import {NavBar} from "./nav-bar/nav-bar";
+import {Dashboard} from "./dashboard/dashboard";
 
 @Component({
   selector: 'app-root',
   imports: [
-    Header
+    Header,
+    NavBar,
+    Dashboard
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
