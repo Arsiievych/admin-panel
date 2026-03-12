0050ed01011ce70943972e696e8c44166276afdc
Pavlo Arsiievych <pavlo.arsiievych@onix-systems.com>
2026-03-12 15:48:44 +0200

    feat(navigation): Add navigation component with structure and styles, integrate into app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import {Component} from "@angular/core";
 import {Header} from "./header/header";
+import {Navigation} from "./navigation/navigation";
 
 @Component({
   selector: 'app-root',
   imports: [
     Header,
+    Navigation,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
