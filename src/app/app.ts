a2747d01e9e5a82254b1233511cc5e0e8c16b342
Pavlo Arsiievych <pavlo.arsiievych@onix-systems.com>
2026-03-12 17:54:49 +0200

    feat(navigation): Update item selector and styles, add navigation-item content to app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import {Component} from "@angular/core";
 import {Header} from "./header/header";
 import {Navigation} from "./navigation/navigation";
+import {Item} from "./navigation/item/item";
 
 @Component({
   selector: 'app-root',
   imports: [
     Header,
     Navigation,
+    Item,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
