21df065ddf5eaea3b85f33295c75fc41bdffe66e
Pavlo Arsiievych <pavlo.arsiievych@onix-systems.com>
2026-03-12 17:42:43 +0200

    feat(navigation): Add item component with styles and bindings for active and muted states

---
+++ src/app/navigation/item/item.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  imports: [],
+  templateUrl: './item.html',
+  styleUrl: './item.css',
+})
+export class Item {
+  @Input() label = '';
+  @Input() active = false;
+  @Input() muted = false;
+}
