b2015f18aab670ac21fc596e8f36974af46e75f4
Pavlo Arsiievych <pavlo.arsiievych@onix-systems.com>
2026-03-12 18:24:59 +0200

    feat(page-shell): Introduce reusable page layout component with styles and structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 import {Header} from "./header/header";
 import {Navigation} from "./navigation/navigation";
 import {NavigationItem} from "./navigation/navigation-item/navigation-item";
+import {PageShell} from "./pages/page-shell/page-shell";
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import {NavigationItem} from "./navigation/navigation-item/navigation-item";
     Header,
     Navigation,
     NavigationItem,
+    PageShell,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
---
+++ src/app/pages/page-shell/page-shell.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-page-shell',
+  imports: [],
+  templateUrl: './page-shell.html',
+  styleUrl: './page-shell.css',
+})
+export class PageShell {
+  @Input() title = '';
+}
