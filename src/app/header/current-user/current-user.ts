f6816d4a08e4d594aeddf281817a066421c3ac62
Pavlo Arsiievych <pavlo.arsiievych@onix-systems.com>
2026-03-13 12:52:15 +0200

    feat(header): Add current-user component with structure and styles

---
+++ src/app/header/current-user/current-user.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-user',
+  imports: [],
+  templateUrl: './current-user.html',
+  styleUrl: './current-user.css',
+})
+export class CurrentUser {
+
+}
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import {Logo} from "../logo/logo";
+import { CurrentUser } from './current-user/current-user';
+import { Logo } from '../logo/logo';
 
 @Component({
   selector: 'app-header',
   imports: [
-    Logo
+    Logo,
+    CurrentUser,
   ],
   templateUrl: './header.html',
   styleUrl: './header.css',
