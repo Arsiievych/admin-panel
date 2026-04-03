afe5ebb2c9f22d6810bbac458d93465496799dc1
Pavlo Arsiievych <pavlo.arsiievych@onix-systems.com>
2026-04-03 18:37:21 +0300

    feat(auth): Implement redesigned login page with form and styles

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Logo } from '../../../shared/ui/logo/logo';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [Logo],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
